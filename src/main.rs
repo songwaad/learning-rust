5a77930b06b7ec1dde53ea456dc2cbe8707c9d2a
Songwaad <Songw44d@gmail.com>
2025-05-06 17:00:59 +0700

    Variable : immutable and mutable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    // immutable
+    let x = 5;
+    println!("x : {}", x);
+
+    // mutable
+    let mut y = 10;
+    y = 15;
+    println!("y : {}", y);
 }
