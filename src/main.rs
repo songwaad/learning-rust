b72d16ddee7ee40e1aa4d2b365b3dc9c93277012
Songwaad <Songw44d@gmail.com>
2025-05-06 17:19:42 +0700

    Data Types : Tuple and Array

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,9 @@
 fn main() {
-    let a:i32 = -1;
-    let b:u8 = 8;
-    println!("a : {}", a);
-    println!("b : {}", b);
+    let tup:(i32, f64, char) = (50, 6.4, 'a');
+    let (x, y, z) = tup;
+    println!("Tuple : {}, {}, {}", x , y, tup.2);
 
-    let pi:f32 = 3.14;
-    println!("pi : {}", pi);
-
-    let okay:bool = false;
-    println!("okay : {}", okay);
-
-    let c:char = 'b';
-    println!("c : {}", c);
+    let arr:[i32; 3] = [1, 2, 3];
+    println!("Array : {}", arr[1]);
 
 }
