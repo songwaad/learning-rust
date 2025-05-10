5de88d306935b9d6082564ee5bf4cf2ebc0169b8
Songwaad <Songw44d@gmail.com>
2025-05-10 23:41:16 +0700

    Bitwise Operators

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 fn main() {
-    let t:bool = true;
-    let f:bool = false;
+    let a:u16 = 2;
+    let b:u16 = 3;
 
-    let result_and:bool = t && f;
-    let result_or:bool = t || f;
-    let result_not:bool = !t;
+    let result_and:u16 = a & b;
+    let result_or:u16 = a | b;
+    let result_xor:u16 = a ^ b;
+
+    println!("{}", result_and);
+    println!("{}", result_or);
+    println!("{}", result_xor);
 
 
 }
