906c35e0eebcf0d42de084166b78db0f71c9e7e6
Songwaad <Songw44d@gmail.com>
2025-05-10 23:34:53 +0700

    Operator

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 fn main() {
-    let mut v = Vec::new();
-    v.push('a');
-    v.push('b');
-
-    for i in &v {
-        print!("{}", i);
-    }
-
-    println!();
+    let sum:u16 = 5 + 10;
+    let dif:f64 = 5.3 - 2.1;
+    let product:i32 = 5 * 10;
+    let quotient:f64 = 10.4 / 2.3;
+    let remainder:u16 = 10 & 3;
 }
