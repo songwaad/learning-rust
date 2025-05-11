3122773ed5a563b0cc7001dbc37263c89fc109fb
Songwaad <Songw44d@gmail.com>
2025-05-11 23:37:35 +0700

    Control Flow : match

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 fn main() {
-    let number:u16 = 7;
+    let number:u16 = 4;
 
-    if number < 5 {
-        println!("The number is less than 5");
-    } else if number == 5 {
-        println!("The number is qual to 5");
-    } else {
-        println!("The number is greater than 5");
+    match number {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("The number is sonething else"),
     }
 }
