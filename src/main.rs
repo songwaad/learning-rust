7b4fc1a60d683b0d941d83ba019b6d2fa2a72bae
Songwaad <Songw44d@gmail.com>
2025-05-13 07:39:18 +0700

    Ownership

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
-    for number in 1..4 {
-        println!("The number is : {}", number);
-    }
+    let s1:String = String::from("hello\n");
+    let s2:String = s1;
+
+    // println!("s1: {}", s1);
+    print!("s2: {}", s2);
 }
