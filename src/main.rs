c1fd2a7d5c79aff15f73be27b1cfa017145cef93
Songwaad <Songw44d@gmail.com>
2025-05-13 08:17:29 +0700

    Error Handling : Result<T, E>

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,16 @@
 fn main() {
-    let msg1:Message = Message::Quit;
-    let msg2:Message = Message::Move { x: 10, y: 20 };
-    let msg3:Message = Message::Write("Hello".to_string());
-    let msg4:Message = Message::ChangeColor(255, 255, 0);
+    let result = divide(10.0, 0.0);
 
-    print_message(msg4);
+    match result {
+        Ok(value) => println!("Result: {}", value),
+        Err(e) => println!("Error: {}", e),
+    }
 }
 
-enum Message{
-    Quit,
-    Move { x:i32, y:i32 },
-    Write(String),
-    ChangeColor(i32, i32, i32),
-}
-
-fn print_message(msg:Message) {
-    match msg {
-        Message::Quit => println!("Quit message"),
-        Message::Move { x, y } => println!("Move to coordinates: {}, {}", x, y),
-        Message::Write(text) => println!("Write message : {}", text),
-        Message::ChangeColor(r, g, b) => println!("Change color to RGB : {}, {}, {}", r, g, b),
+fn divide(numerator: f64, denominator: f64) -> Result<f64, String> {
+    if denominator == 0.0 {
+        Err("Cannot divivde by zero".to_string())
+    } else {
+        Ok(numerator / denominator)
     }
 }
