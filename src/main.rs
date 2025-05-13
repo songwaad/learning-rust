f1e1e451d4c94aea89bd4413f70c85bf4228d70f
Songwaad <Songw44d@gmail.com>
2025-05-13 07:56:00 +0700

    Structs : Unit Structs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 fn main() {
-    let black:Color = Color(0,0,0);
-    print!("Black color RGB : {}, {}, {}", black.0, black.1, black.2);
+    let subject:AlwayEqual = AlwayEqual;
 }
 
-struct Color(i32, i32, i32);
+struct AlwayEqual;
